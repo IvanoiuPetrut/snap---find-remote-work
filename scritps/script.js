8f31c8ca16b84caa0d1e009f5cefb7ea5de9c31e
Petrut <ivanoiu.petrut27@yahoo.com>
2022-05-13 00:20:14 +0300

    added the project

---
+++ scritps/script.js
@@ -0,0 +1,13 @@
+// CHANGE HERO IMG SOURCE FOR MOBILE
+// const observer = new IntersectionObserver(
+//   function (entries) {
+
+// const heroImg = document.getElementsByClassName("hero--img");
+// let viewportHeight = window.innerHeight;
+// let viewportWidth = window.innerWidth;
+
+// console.log(viewportHeight, viewportWidth);
+// if (viewportWidth <= 800) {
+//   // heroImg.src = "images/image-hero-mobile.png";
+//   console.log("mobile");
+// }
